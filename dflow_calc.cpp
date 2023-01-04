a56f1d6c8fd118a84184b0d5d98946cc0d0b99f9
edenl <edenlevi36@gmail.com>
2023-01-04 16:56:47 +0200

    push

--- dflow_calc.cpp
+++ dflow_calc.cpp
@@ -10,17 +10,31 @@ public:
     std::vector<Node*> childrenNodes; // nodes that depend on this node
     Node* dLeft;
     Node* dRight;
+
+    Node(InstInfo dataIn) : dLeft(nullptr), dRight(nullptr) {
+        this->data.dstIdx = dataIn.dstIdx;
+        this->data.opcode = dataIn.opcode;
+        this->data.src1Idx = dataIn.src1Idx;
+        this->data.src2Idx = dataIn.src2Idx;
+    }
 };
 
 class ProgramContext {
 public:
+    unsigned int numOfInsts;
     std::vector<Node*> nodes;
     std::vector<Node*> exitNodes; // nodes no one depend on
+
+    ProgramContext(unsigned int numOfInsts) : numOfInsts(numOfInsts) {}
 };
 
 ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[], unsigned int numOfInsts) {
 
+    ProgramContext* prog = new ProgramContext(numOfInsts);
 
+    for(int i = 0; i < numOfInsts; i++) {
+       prog->nodes.push_back()  progTrace[i];
+    }
 
     return PROG_CTX_NULL;
 }
