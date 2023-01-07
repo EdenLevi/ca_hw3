8006652ffa2a112d031d3a1ec6f8933f9329e5d4
w0lfl0w <pazwolfwolfpaz@gmail.com>
2023-01-07 18:48:13 +0200

    added dflow_calc.cpp

--- dflow_calc.cpp
+++ dflow_calc.cpp
@@ -43,7 +43,7 @@ ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[],
     ProgramContext *prog = new ProgramContext(numOfInsts);
 
     for (int i = 0; i < numOfInsts; i++) {
-        /// read a command from input and
+        /// read a command from input and check its dependencies
         Node *node = new Node(progTrace[i], i);
         prog->nodes.push_back(node);
 
@@ -57,7 +57,9 @@ ProgCtx analyzeProg(const unsigned int opsLatency[], const InstInfo progTrace[],
 
         /// updating cycles time
         if(node->dLeft && node->dRight) {
-            node->cyclesIncludingCommand = max(node->dLeft->cyclesIncludingCommand, node->dRight->cyclesIncludingCommand) + opsLatency[node->data.opcode];
+            node->cyclesIncludingCommand =
+                    max(node->dLeft->cyclesIncludingCommand, node->dRight->cyclesIncludingCommand)
+                                                                   + opsLatency[node->data.opcode];
         }
         else if(node->dLeft) {
             node->cyclesIncludingCommand = node->dLeft->cyclesIncludingCommand + opsLatency[node->data.opcode];
@@ -86,11 +88,23 @@ int getInstDepth(ProgCtx ctx, unsigned int theInst) {
 }
 
 int getInstDeps(ProgCtx ctx, unsigned int theInst, int *src1DepInst, int *src2DepInst) {
-    return -1;
+    ProgramContext* prog = ((ProgramContext*)ctx);
+    if(theInst >= prog->nodes.size()) return -1; // check if there is command number <theInst>
+    *src1DepInst = (prog->nodes[theInst]->dLeft ? prog->nodes[theInst]->dLeft->id : -1);
+    *src2DepInst = (prog->nodes[theInst]->dRight ? prog->nodes[theInst]->dRight->id : -1);
+
+    return 0;
 }
 
 int getProgDepth(ProgCtx ctx) {
-    return 0;
+
+    ProgramContext* prog = ((ProgramContext*)ctx);
+    int progMaxDepth = 0;
+    for(Node* n : prog->nodes) {
+        progMaxDepth = max(progMaxDepth, n->cyclesIncludingCommand);
+    }
+
+    return progMaxDepth;
 }
 
 
